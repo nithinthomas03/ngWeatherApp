32dc6ac3520c9f61e39d6c103287727bbff3032e
nmathewt <nmthomas21@gmail.com>
2017-01-30 11:02:26 -0800

    AJAX request changes

--- controller.js
+++ controller.js
@@ -11,12 +11,12 @@ myapp.controller("forecastController",["$scope",'cityService','weatherService',f
 
 	var cnt = 7;
 	$scope.cityName = cityService.name;
-	$scope.wResult = weatherService.weatherFunc($scope.cityName,cnt);
-	var wObj=$scope.wResult;
-	console.log(wObj);
-	//$scope.wIcon = $scope.wResult.list[0].weather[0].icon;
-	//console.log($scope.wIcon);
-	
+	var wRes = weatherService.weatherFunc($scope.cityName,cnt).$promise.then(function(resp){
+		$scope.wResult = resp;
+		console.log($scope.wResult);
+		$scope.wIcon = $scope.wResult.list[0].weather[0].icon;
+	});
+
 	
 	$scope.k2F = function(tempK){
 		return Math.round((9/5*(tempK - 273)+32));
